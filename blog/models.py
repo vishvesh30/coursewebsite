4f6a780b9eca397d783f79fe7580cadd70c3bf75
vishvraj <kapadia.vishvesh@gmail.com>
2017-07-23 23:24:09 +0530

    with blog

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class post(models.Model):
     title=models.CharField(max_length=200)
     data=models.TextField()
-    date=models.DateField()
+    date=models.CharField(max_length=13)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from blog.models import post
 # Create your views here.
+def create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        data=request.POST.get('data')
+        date=request.POST.get('date')
+        post_object = post(title=title,data=data,date=date)
+        post_object.save()
+        return render(request, 'form.html', {'message': 'success'})
+    else:
+        return render(request, 'form.html')
